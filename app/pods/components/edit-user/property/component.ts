2264e64526fa0783651c3c9481e64d77dd4dc7b6
Dawid Damek <dawid.damek98@gmail.com>
2022-09-07 19:05:42 +0200

    feat: edit user data

---
+++ app/pods/components/edit-user/property/component.ts
@@ -0,0 +1,62 @@
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import SessionService from 'service-booking-ts/pods/session/service';
+import User from 'service-booking-ts/pods/user/model';
+
+interface EditUserPropertyArgs {
+  userProperty: keyof User;
+}
+
+type editableUserTypes = Omit<User, 'bikes' | 'orders' | 'comments'>;
+
+export default class EditUserProperty extends Component<EditUserPropertyArgs> {
+  @service declare session: SessionService;
+  @tracked isEditable = false;
+  @tracked userProperty = this.args.userProperty;
+
+  get shouldNotEdit() {
+    return !this.isEditable;
+  }
+
+  get currentUser() {
+    return this.session.currentUser;
+  }
+
+  get property() {
+    return this.args.userProperty;
+  }
+
+  @action
+  onSave(event: TransitionEvent) {
+    event.preventDefault();
+    const target = event.target as HTMLInputElement;
+    target.focus();
+    this.currentUser.save();
+    this.#editSwitch();
+  }
+
+  @action
+  onCancel() {
+    this.currentUser.rollbackAttributes();
+    this.#editSwitch();
+  }
+
+  @action
+  onEditPropertySwitch() {
+    this.#editSwitch();
+  }
+
+  @action
+  onPropertyChange<T extends keyof editableUserTypes>(
+    key: T,
+    { target }: { target: HTMLInputElement }
+  ) {
+    this.currentUser[key] = target.value as User[T];
+  }
+
+  #editSwitch() {
+    this.isEditable = !this.isEditable;
+  }
+}
