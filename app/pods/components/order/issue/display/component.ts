ea18d885235bc7b786830e739eb9442ed66907f1
Dawid Damek <dawid.damek98@gmail.com>
2022-10-03 19:07:06 +0200

    feat: allow admin to change statuses of issues

--- app/pods/components/order/issue/display/component.ts
+++ app/pods/components/order/issue/display/component.ts
@@ -1,5 +1,6 @@
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import Issue from 'service-booking-ts/pods/issue/model';
 
 interface OrderIssueDisplayComponentArgs {
@@ -8,9 +9,27 @@ interface OrderIssueDisplayComponentArgs {
 }
 
 export default class OrderIssueDisplayComponent extends Component<OrderIssueDisplayComponentArgs> {
+  @tracked isEditing = false;
+  @tracked selected = this.args.issue.status;
+  issueStatuses = ['In progress', 'Done', 'Rejected'];
+
   @action
   onDeleteIssue() {
     const { issue, deleteRecord } = this.args;
     deleteRecord(issue);
+  }
+
+  @action
+  onEditStatus() {
+    this.isEditing = !this.isEditing;
+  }
+
+  @action
+  async onChange(selected: string) {
+    const { issue } = this.args;
+    this.selected = selected;
+    issue.set('status', selected);
+    await issue.save();
+    this.isEditing = !this.isEditing;
   }
 }
