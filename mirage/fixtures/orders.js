82353633fdebce0e70828d6b3e24fbf146cd9095
Dawid Damek <dawid.damek98@gmail.com>
2022-10-21 16:05:59 +0200

    wip: mirage add fixtures and application serializer

---
+++ mirage/fixtures/orders.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    type: 'Service',
+    status: 'New',
+    acceptanceDate: new Date(2022, 10, 10),
+    releaseDate: new Date(2022, 10, 15),
+    ownerId: 1,
+    bike: 1,
+  },
+];
---
+++ mirage/fixtures/users.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    id: 1,
+    name: 'DD',
+    surname: 'Admin',
+    isAdmin: 'true',
+    email: 'dawid@admin.com',
+    phoneNumber: '666666666',
+  },
+];
